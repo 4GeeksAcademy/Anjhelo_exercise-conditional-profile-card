efe8033bdebcea821ed23f440a4a193d6b200f4c
LOHAN2000 <160059524+LOHAN2000@users.noreply.github.com>
2024-08-02 16:40:57 +0000

    generador_perfil

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,57 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let nombre = variables.name == null ? "No Name" : variables.name;
+  let apellido =
+    variables.lastName == null ? "No LastName" : variables.lastName;
+
+  let position =
+    variables.socialMediaPosition == "position-left"
+      ? (position = "position-left")
+      : (position = "position-right");
+
+  let rol = variables.role == null ? (rol = "No job") : (rol = variables.role);
+
+  let ciudad =
+    variables.city == null ? (ciudad = "No city") : (ciudad = variables.city);
+
+  let pais =
+    variables.country == null
+      ? (pais = "No country")
+      : (pais = variables.country);
+
+  let x =
+    variables.twitter == null
+      ? "https://x.com"
+      : `https://x.com/${variables.twitter}`;
+
+  let gh =
+    variables.github == null
+      ? "https://github.com"
+      : `https://github.com/${variables.github}`;
+
+  let lkd =
+    variables.linkedin == null
+      ? "https://linkedin.com"
+      : `https://linkedin.com/${variables.linkedin}`;
+
+  let ins =
+    variables.instagram == null
+      ? "https://instagram.com"
+      : `https://instagram.com/${variables.instagram}`;
   // reset the website body with the new html output
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${nombre} ${apellido}</h1>
+          <h2>${rol}</h2>
+          <h3>${ciudad}, ${pais}</h3>
+          <ul class="${position}">
+            <li><a href="${x}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${gh}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="${lkd}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${ins}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
